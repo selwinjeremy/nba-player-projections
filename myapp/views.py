c252cd0852ae83bd3e3529f0d582f7e91d20822a
selwinjeremy <jcselwin@gmail.com>
2025-02-08 13:25:49 -0500

    Added player stats

--- myapp/views.py
+++ myapp/views.py
@@ -5,8 +5,8 @@ from nba_api.stats.endpoints.scoreboardv2 import ScoreboardV2
 from nba_api.stats.endpoints.leaguegamelog import LeagueGameLog
 
 from nba_api.stats.endpoints.teaminfocommon import TeamInfoCommon
-from nba_api.stats.endpoints.teamgamelogs import TeamGameLogs
 
+from nba_api.stats.endpoints.teamplayerdashboard import TeamPlayerDashboard
 import pandas as pd
 
 # Create your views here.
@@ -63,25 +63,8 @@ def game_list(request):
 
     return JsonResponse(final_df.to_dict(orient="records"),safe=False)
 
-def game_detail(request, game_id):
 
-    #Game Data
-    games = ScoreboardV2()
-    games_dict = games.get_dict()
-    data = games_dict['resultSets']
-
-    game_header_data = data[0]
-    
-    game_header_df = pd.DataFrame(game_header_data['rowSet'], columns=game_header_data['headers'])
-    game_header_df['GAME_ID'] = game_header_df['GAME_ID'].astype(str)
-
-
-    game_header_df = game_header_df[game_header_df['GAME_ID'] == game_id]
-    
-    home_team_id = game_header_df.iloc[0]['HOME_TEAM_ID']
-    away_team_id = game_header_df.iloc[0]['VISITOR_TEAM_ID']
-
-    #Teams like City, Team Name, Record, Win Pct
+def get_teams_data(home_team_id: str, away_team_id: str):
     home_team = TeamInfoCommon(team_id=home_team_id)
     away_team = TeamInfoCommon(team_id=away_team_id)
     home_team = home_team.get_dict()
@@ -104,10 +87,10 @@ def game_detail(request, game_id):
         'TEAM_ID', 'TEAM_CITY', 'TEAM_NAME', 'W', 'L', 'PCT',
         'PTS_RANK', 'PTS_PG', 'OPP_PTS_RANK', 'OPP_PTS_PG'
     ]]
+    return final_df.to_dict(orient="records")
 
-    #Past Matchup Data
-    # Filter for games where the teams played against each other (both home/away combinations)
 
+def get_matchups(home_team_id: str, away_team_id: str):
     game_logs = LeagueGameLog()
     game_logs = game_logs.get_dict()
     data = game_logs['resultSets']
@@ -125,11 +108,70 @@ def game_detail(request, game_id):
     away_stats = away_stats.rename(columns={col: f'AWAY_{col}' for col in stat_columns})
 
     merged_stats = pd.merge(home_stats, away_stats, on='GAME_ID', suffixes=('_HOME', '_AWAY'))
+    return merged_stats.to_dict(orient="records")
 
+
+def player_analysis(team_id: str):
+    player_stats = TeamPlayerDashboard(team_id=team_id)
+    player_stats = player_stats.get_dict()
+    data = player_stats['resultSets']
+    player_stats_data = data[1]
+    player_stats_df = pd.DataFrame(player_stats_data['rowSet'], columns=player_stats_data['headers'])
+    player_stats_df['MIN'] = (player_stats_df['MIN'] / player_stats_df['GP']).round(1)
+    player_stats_df['FGM'] = (player_stats_df['FGM'] / player_stats_df['GP']).round(1)
+    player_stats_df['FGA'] = (player_stats_df['FGA'] / player_stats_df['GP']).round(1)
+    player_stats_df['FG3A'] = (player_stats_df['FG3A'] / player_stats_df['GP']).round(1)
+    player_stats_df['FG3M'] = (player_stats_df['FG3M'] / player_stats_df['GP']).round(1)
+    player_stats_df['REB'] = (player_stats_df['REB'] / player_stats_df['GP']).round(1)
+    player_stats_df['AST'] = (player_stats_df['AST'] / player_stats_df['GP']).round(1)
+    player_stats_df['TOV'] = (player_stats_df['TOV'] / player_stats_df['GP']).round(1)
+    player_stats_df['STL'] = (player_stats_df['STL'] / player_stats_df['GP']).round(1)
+    player_stats_df['BLK'] = (player_stats_df['BLK'] / player_stats_df['GP']).round(1)
+    player_stats_df['PF'] = (player_stats_df['PF'] / player_stats_df['GP']).round(1)
+    player_stats_df['PTS'] = (player_stats_df['PTS'] / player_stats_df['GP']).round(1)
+    player_stats_df['PLUS_MINUS'] = (player_stats_df['PLUS_MINUS'] / player_stats_df['GP']).round(1)
+    player_stats_df = player_stats_df.drop(columns=['GROUP_SET', 'NICKNAME', 'W', 'L', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB','WNBA_FANTASY_PTS', 'GP_RANK', 'W_RANK', 'L_RANK',
+       'W_PCT_RANK', 'MIN_RANK', 'FGM_RANK', 'FGA_RANK', 'FG_PCT_RANK',
+       'FG3M_RANK', 'FG3A_RANK', 'FG3_PCT_RANK', 'FTM_RANK', 'FTA_RANK',
+       'FT_PCT_RANK', 'OREB_RANK', 'DREB_RANK', 'REB_RANK', 'AST_RANK',
+       'TOV_RANK', 'STL_RANK', 'BLK_RANK', 'BLKA_RANK', 'PF_RANK', 'PFD_RANK',
+       'PTS_RANK', 'PLUS_MINUS_RANK', 'NBA_FANTASY_PTS_RANK', 'DD2_RANK',
+       'TD3_RANK', 'WNBA_FANTASY_PTS_RANK', 'NBA_FANTASY_PTS','PFD','BLKA'])
+
+    return player_stats_df.to_dict(orient="records")
+
+
+def game_detail(request, game_id):
+
+    #Game Data
+    games = ScoreboardV2()
+    games_dict = games.get_dict()
+    data = games_dict['resultSets']
+
+    game_header_data = data[0]
+    
+    game_header_df = pd.DataFrame(game_header_data['rowSet'], columns=game_header_data['headers'])
+    game_header_df['GAME_ID'] = game_header_df['GAME_ID'].astype(str)
+
+
+    game_header_df = game_header_df[game_header_df['GAME_ID'] == game_id]
+    
+    home_team_id = game_header_df.iloc[0]['HOME_TEAM_ID']
+    away_team_id = game_header_df.iloc[0]['VISITOR_TEAM_ID']
+
+    #Teams like City, Team Name, Record, Win Pct
+    teams = get_teams_data(home_team_id, away_team_id)
+
+    #Past Matchup Data
+    matchups = get_matchups(home_team_id, away_team_id)
+
+    home_player_stats = player_analysis(home_team_id)
+    away_player_stats = player_analysis(away_team_id)
 
     return render(request, 'game_detail.html', {
         'game_data': game_header_df.to_dict(orient="records"),
-        'team_data': final_df.to_dict(orient="records"),
-        'previous_matchups': merged_stats.to_dict(orient="records")
+        'team_data': teams,
+        'previous_matchups': matchups,
+        'home_team_players': home_player_stats,
+        'away_team_players': away_player_stats
     })
-
